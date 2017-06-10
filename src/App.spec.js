482a660485e3cb9df968bcff1c22998ba6ae7ef4
Nishan Bajracharya <nishanbajracharya@lftechnology.com>
2017-06-10 13:49:44 +0545

    Remove mocha chai testing

--- src/App.spec.js
+++ src/App.spec.js
@@ -1,23 +1,11 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import chai from 'chai';
 
 import App from './App';
 
-// Test using Mocha Chai
-describe('App', () => {
-  it(`should render a H1 tag with value of 'Hello World'`, () => {
-    const output = renderer.create(<App />).toJSON();
-
-    chai.expect(output).to.have.property('type', 'h1');
-    chai.expect(output).to.have.deep.property('children', ['Hello World']);
-  });
-});
-
 // Test using Jest
 test(`should render a H1 tag with value of 'Hello World'`, () => {
-  const tree = renderer.create(
-    <App />
-  ).toJSON();
+  const tree = renderer.create(<App />).toJSON();
+
   expect(tree).toMatchSnapshot();
 });
